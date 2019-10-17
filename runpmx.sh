a193158e8b7d33dc290e64bd10cb60508d844cc2
notis <notis@debian-promox-test.test.snail>
2019-10-18 02:29:20 +0300

    изменено:      runpmx.sh

--- runpmx.sh
+++ runpmx.sh
@@ -22,8 +22,9 @@ destroy_unit () {
 
 cp /etc/hosts /opt/hosts.bck
 echo "127.0.0.1	localhost localhost.localdomain" > /opt/hosts
-EXT_IP=$(tail -n 15 /var/lib/dhcp/dhclient.leases  | grep fixed-address | sed 's/;//; s/.*ss //')
+#EXT_IP=$(tail -n 15 /var/lib/dhcp/dhclient.leases  | grep fixed-address | sed 's/;//; s/.*ss //')
 #EXT_IP=192.168.0.169
+EXT_IP=$(hostname --ip_address)
 echo "$EXT_IP	$(cat /proc/sys/kernel/hostname).$(dnsdomainname) $(cat /proc/sys/kernel/hostname) pvelocalhost" >> /opt/hosts
 
 touch /opt/testfile.tes
