cbc2342c971e04e54abd86f70d586ccb8887415b
notis <notisster@gmail.com>
2019-10-18 02:38:27 +0300

    изменено:      createvm.sh 	изменено:      unit2

--- createvm.sh
+++ createvm.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+PATH="/usr/local/bin:/usr/bin:/bin"
+SHELL="/bin/bash"
+LANG="ru_RU.UTF-8"
 
- echo "HOLA!"
+change_unit () {
+	cp /opt/install/unit2 /etc/systemd/system/pmxrunonce.service
+	/usr/bin/systemctl daemon-reload
+	}
 
+#-----------
+destroy_unit () {
+	/usr/bin/systemctl disable pmxrunonce.service
+	rm /etc/systemd/system/pmxrunonce.service
+	/usr/bin/systemctl daemon-reload
+	}
+#------------
+ 
+touch /opt/testfile22.tes
 
-
-
-
+destroy_unit 
